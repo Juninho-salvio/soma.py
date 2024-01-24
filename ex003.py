eb5479aefb59f94f69a04514d2d52118e6221c4e
Sálvio De Oliveira Santos Júnior <79759829+Juninho-salvio@users.noreply.github.com>
2024-01-24 17:46:56 -0300

    Add files via upload

---
+++ ex003.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+s = n1 + n2
+
+print('A soma do valor \033[1;30;41m{}\033[m e o valor \033[1;30;44m{}\033[m é igual a \033[7m{}\033[m'.format(n1, n2, s))
